43fe2af5659ebfe5cde37464490bd17173a39b59
tomas <t.gonzalezb@duocuc.cl>
2025-05-25 08:03:32 -0400

    Creacion modelo Usuario

--- Cotizador/CotizadorApp/models.py
+++ Cotizador/CotizadorApp/models.py
@@ -1,9 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    rut = models.CharField(max_length=12, unique=True)
+    telefono = models.CharField(max_length=15)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.email}"
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class Cotizacion(models.Model):
-    nombre = models.CharField(max_length=100)
-    email = models.EmailField()
-    detalles = models.TextField()
-    cantidad = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cantidad = models.PositiveIntegerField()
     precio = models.DecimalField(max_digits=10, decimal_places=2)
+    detalles = models.TextField()
     fecha = models.DateTimeField(auto_now_add=True)
--- Cotizador/CotizadorApp/urls.py
+++ Cotizador/CotizadorApp/urls.py
@@ -1,8 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import CotizacionViewSet
+from .views import CotizacionViewSet, UserViewSet
 from django.urls import path
 
 router = DefaultRouter()
 router.register(r'cotizaciones', CotizacionViewSet, basename='cotizacion')
+router.register(r'usuarios', UserViewSet, basename='usuario')
 
 urlpatterns = router.urls
