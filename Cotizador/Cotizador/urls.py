b08b2f2c9040028cab524431dc6730f4b9caa502
tomas <t.gonzalezb@duocuc.cl>
2025-05-28 17:11:04 -0400

    se añade urls de exportacion

--- Cotizador/Cotizador/urls.py
+++ Cotizador/Cotizador/urls.py
@@ -17,10 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from CotizadorApp.exports import ExportarCotizacionesExcel, ExportarCotizacionPDF
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("CotizadorApp.urls")),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('export/cotizaciones/', ExportarCotizacionesExcel.as_view(), name='export-cotizaciones'),
+    path('export/cotizacion/<int:pk>/pdf/', ExportarCotizacionPDF.as_view(), name='export-cotizacion-pdf'),
 ]
